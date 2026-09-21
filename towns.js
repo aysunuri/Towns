2faedc8ec46c997ddec8e4bbaa8a6dfbbeb1bf7c
Aysu Nuri <aysuertan4@gmail.com>
2026-09-21 16:21:18 +0300

    Add new town functionality

--- towns.js
+++ towns.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$('#btnDelete').click(deleteTown)
+	$('#btnAdd').click(addTown)
 });
 
 function deleteTown() {
@@ -16,4 +17,15 @@ function deleteTown() {
 		$('#result').text(townName + " deleted.");
 	else
 		$('#result').text(townName + " not found.");
+}
+
+function addTown() {
+	let townName = $('#newTownName').val();
+	$('#newTownName').val('');
+	if (townName.trim() === '') {
+		$('#result').text("Please enter a town name.");
+		return;
+	}
+	$('#towns').append($('<option>').text(townName));
+	$('#result').text(townName + " added.");
 }
